f70fc1800e5f4ccbca2beb045a300541d1d603a4
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-07-30 09:43:18 +0530

    Update log.py

--- log.py
+++ log.py
@@ -1,7 +1,14 @@
+import os
+
+API_ID = 12621397
+API_HASH = "e68f9fdc9c8e562d9dab37ac3731a7b6"
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+PORT = os.environ.get("PORT", "8080")
+
 import logging
 from logging.handlers import RotatingFileHandler
-import os
-PORT = os.environ.get("PORT", "8080")
+
 LOG_FILE_NAME = "AnimeRobots.txt"
 logging.basicConfig(
     level=logging.INFO,
