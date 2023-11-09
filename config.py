6ddb82682251db46fb18809f2b6c37d8cadd43b6
Hyuu <miyamuraizumi3dsp@gmail.com>
2023-11-09 08:37:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,16 +12,14 @@ OWNER_ID = 5329765587
 ADMINS = []
 if not 1497264683 in ADMINS:  #Shiro
   ADMINS.append(1497264683)
-if not 5024928504 in ADMINS:  # mei
-  ADMINS.append(5024928504)
 if not 1302714537 in ADMINS:  # DSp
   ADMINS.append(1302714537)
-if not 1225219091 in ADMINS:  # argha
-  ADMINS.append(1225219091)
 if not 5296520170 in ADMINS:  # emi
   ADMINS.append(5296520170)
 if not 5329765587 in ADMINS:  # schwi
   ADMINS.append(5329765587)
+if not 5912572748 in ADMINS:  # saber
+  ADMINS.append(5912572748)
 
 
 
