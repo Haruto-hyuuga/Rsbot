cc052692f09a243b281fba945dec07fe66ab2766
Shiro <miyamuraizumi3dsp@gmail.com>
2023-12-06 22:54:52 +0530

    Create Welc.py

---
+++ HELPER/Welc.py
@@ -0,0 +1,18 @@
+from random import choice
+from PIL import Image, ImageDraw
+BASE_IMG = [
+  'Base/WP01', 
+  'Base/WP02', 
+  'Base/WP03',
+  'Base/WP04', 
+  'Base/WP05', 
+  'Base/WP06', 
+  'Base/WP07', 
+  'Base/WP08', 
+  'Base/WP09', 
+  'Base/WP10', 
+  'Base/WP11', 
+  'Base/WP12'
+]
+
+async def gen_wlcm():
