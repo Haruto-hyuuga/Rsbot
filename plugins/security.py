f91d28981b74135f5937bc170cb4828d8645c319
Shiro <miyamuraizumi3dsp@gmail.com>
2023-12-07 14:55:58 +0530

    Update security.py

--- plugins/security.py
+++ plugins/security.py
@@ -107,7 +107,7 @@ async def welcome_sec1(app: app, message: Message):
                 if not await present_user(member.id): await add_user(member.id)
             wlcm_pic = await gen_wlcm(app, member)
             if message.from_user.id != member.id:
-                wlcm_txt += f"\n\n🫂 Thanks {message.from_user.mention} for inviting {member.mention}"
+                wlcm_txt += f"\n<pre>🫂 Thanks {message.from_user.mention} for inviting {member.mention}</pre>"
             await app.send_photo(
                 chat_id=message.chat.id,
                 photo=wlcm_pic,
