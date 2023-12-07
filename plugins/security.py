7f9e67d457ed7430659d3022fd4de30556ba9333
Shiro <miyamuraizumi3dsp@gmail.com>
2023-12-07 20:13:32 +0530

    Update security.py

--- plugins/security.py
+++ plugins/security.py
@@ -84,19 +84,21 @@ async def welcome_sec1(app: app, message: Message):
             return
         if message.new_chat_member.status == ChatMemberStatus.BANNED: return
         if member:
-            RESTRICTED = await message.chat.restrict_member(
-                user_id=member.id,
-                permissions=ChatPermissions(
-                    can_send_messages=True,
-                    can_invite_users=True,
-                    can_send_polls=True,
-                    can_add_web_page_previews=False,
-                    can_send_other_messages=False,
-                    can_send_media_messages=False,
-                    can_change_info=False,
-                    can_pin_messages=False,
-                ),
-            )
+            RESTRICTED = False
+            if member.id not in WHITE_LIST:
+                RESTRICTED = await message.chat.restrict_member(
+                    user_id=member.id,
+                    permissions=ChatPermissions(
+                        can_send_messages=True,
+                        can_invite_users=True,
+                        can_send_polls=True,
+                        can_add_web_page_previews=False,
+                        can_send_other_messages=False,
+                        can_send_media_messages=False,
+                        can_change_info=False,
+                        can_pin_messages=False,
+                    ),
+                )
             Username = f"@{member.username}" if member.username else f"{member.mention}"
             invkeyar = Ronvkeyar
             X, Y, Z = hearts()
