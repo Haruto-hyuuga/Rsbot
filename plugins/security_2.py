a0316acb518a6eef78e7f9f74cd70f285ca0520c
Shiro <miyamuraizumi3dsp@gmail.com>
2024-01-02 19:08:45 +0530

    Update security_2.py

--- plugins/security_2.py
+++ plugins/security_2.py
@@ -16,7 +16,7 @@ Ronvkeyar = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(text="🏛️ RULES", url="https://telegra.ph/ANIME-HINDI-COMMUNITY-RULES-01-02"),
-            InlineKeyboardButton(text=" 🌐", url="https://t.me/AnimeCommunityChat"),
+            InlineKeyboardButton(text="🌐", url="https://t.me/AnimeCommunityChat"),
         ]
     ]
 )
