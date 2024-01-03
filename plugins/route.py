d2146f9c6a522a52222bc46c8938f872b10d0475
Shiro <miyamuraizumi3dsp@gmail.com>
2024-01-03 13:22:48 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,4 +4,13 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Animerobots")
+    html_content = """
+    <html>
+        <body>
+            <h1>Welcome to Animerobots</h1>
+            <img src="Base/PFPZ/WPnone.jpg" alt="Your Image">
+        </body>
+    </html>
+    """
+    return web.Response(text=html_content, content_type="text/html")
+    #return web.json_response("Animerobots")
